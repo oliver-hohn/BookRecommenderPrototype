3e14d96245ef766e1fa67e73a6bbf97df817e04e
Oliver <oliver.hohn@kcl.ac.uk>
2016-12-03 19:44:46 +0000

    HomeActivity.java: -updated from the previous version, now uses a sliding in menu. -this menu has the options, About, Account, Settings and  Logout. -only Logout is implemented, the rest just show a "Feature Coming soon" toast.
    home.xml:
    -the menu for the items on the toolbar, ie Basket.

--- app/src/main/java/oliverhohn/bookrecommender/HomeActivity.java
+++ app/src/main/java/oliverhohn/bookrecommender/HomeActivity.java
@@ -1,17 +1,21 @@
 package oliverhohn.bookrecommender;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
-    private final static String TAG = "HomeActivity";
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "HomeActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,26 +23,80 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setTitle("Logged In as:");
-        getSupportActionBar().setSubtitle("Oliver Hohn @ Maughan Library");
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.home_menu,menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id){
             case R.id.action_basket:
-                //User picked the basket icon
-                Log.d(TAG,"Pressed Basket");
+                Log.d(TAG, "Clicked on basket");
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        switch (id){
+            case R.id.nav_about:
+                Log.d(TAG , "About Navigation pressed");
+                showToast("Feature coming soon");
+                break;
+            case R.id.nav_account:
+                Log.d(TAG , "Account Navigation pressed");
+                showToast("Feature coming soon");
+                break;
+            case R.id.nav_settings:
+                Log.d(TAG , "Settings Navigation pressed");
+                showToast("Feature coming soon");
+                break;
+            case R.id.nav_logout:
+                Log.d(TAG , "Log out Navigation pressed");
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                break;
+            default:
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
